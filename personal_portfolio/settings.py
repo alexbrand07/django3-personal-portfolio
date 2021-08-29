bb03f2080697e09846bee0a5b011853f4006b0b8
Alexey Khorishko <alexeykhorishko@Alexeys-MacBook-Pro.local>
2021-08-29 14:06:22 +0100

    local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -132,4 +132,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 try:
     from .local_settings import *
 except ImportError:
-    print("Looks like no local file. You must be on production")
+    print("Looks like no local file. You must be on production right now")
